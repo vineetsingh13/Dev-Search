8bc4a72887e8cf08c977a0ba4bda64235692a560
vineetsingh13 <singhvineet13502@gmail.com>
2024-08-03 00:57:05 +0530

    passed some objects as dictionary through the render method to access them in the html pages. Implemented lists and conditions and links in the html pages

--- projects/views.py
+++ projects/views.py
@@ -2,10 +2,40 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+projectslist=[
+    {
+        'id': '1',
+        'title': "Ecommerce Website",
+        'description': "Fully functional ecommerce website"
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': "Project where i built my portfolio website"
+    },
+    {
+        'id': '3',
+        'title': "Social Website",
+        'description': "A fully functional ecommerce website"
+    }
+]
+#so to pass data to the html pages we pass them as dictionary in the render method
+#and we access them with {{page}} in the html pages
 def projects(request):
-    return render(request, 'projects/projects.html') 
+    page="projects"
+    number=10
+    context ={'page':page,'number':number,'projects': projectslist}
+    return render(request, 'projects/projects.html',context) 
+
 
 #whatever paramter we name in the url path
 #the same name should be used as parameter in function
 def project(request,pk):
-    return render(request, 'projects/single-project.html')  
+    projectObj=None
+
+    for i in projectslist:
+        if i['id']==pk:
+            projectObj=i
+
+    
+    return render(request, 'projects/single-project.html',{'project':projectObj})  
