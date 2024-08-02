d019e50a2a2435e40f2cad62e8129601ddf0b5ee
vineetsingh13 <singhvineet13502@gmail.com>
2024-08-02 18:06:33 +0530

    created a templates folder that contains the main page template. Created a template in the projects app and added those html files in it. Now in the views.py rendered those html pages. In the settings.py added the templates directory

--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse("Here are our products") 
+    return render(request, 'projects/projects.html') 
 
 #whatever paramter we name in the url path
 #the same name should be used as parameter in function
 def project(request,pk):
-    return HttpResponse("Single project"+' '+str(pk)) 
+    return render(request, 'projects/single-project.html')  
