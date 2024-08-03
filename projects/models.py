bd4860e242dfc5cd4db448a84744237a1a75d976
vineetsingh13 <singhvineet13502@gmail.com>
2024-08-03 16:46:22 +0530

    created the review, tag table and registered them in admin

--- projects/models.py
+++ projects/models.py
@@ -9,6 +9,9 @@ class Project(models.Model):
     description=models.TextField(null=True,blank=True)
     demo_link=models.CharField(max_length=2000,null=True,blank=True)
     source_link=models.CharField(max_length=2000,null=True,blank=True)
+    
+    #if the tag model is written before the Project model we dont need to use '' else we need to use ''
+    tags=models.ManyToManyField('Tag',blank=True)
     created=models.DateTimeField(auto_now_add=True)
     id=models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
     
@@ -17,4 +20,27 @@ class Project(models.Model):
         return self.title
         
 
+class Review(models.Model):
+    Vote_type=(
+        ('up', 'Up Vote'),
+        ('down','Down Vote')
+    )
 
+    #so using cascade once the project is deleted all the associated reviews are also deleted
+    project=models.ForeignKey(Project,on_delete=models.CASCADE)
+    body=models.TextField(null=True,blank=True)
+    #now the value attribute below is going to be a dropdown list
+    value=models.CharField(max_length=200,choices=Vote_type)
+    created=models.DateTimeField(auto_now_add=True)
+    id=models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name=models.CharField(max_length=200)
+    created=models.DateTimeField(auto_now_add=True)
+    id=models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
+
+    def __str__(self):
+        return self.name
