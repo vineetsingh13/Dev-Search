7c712f10e940a66a3fc26350b52640a3ab596179
vineetsingh13 <singhvineet13502@gmail.com>
2024-08-04 18:49:42 +0530

    learned about querying the database. In views.py change the static data to dynamic data from the database.

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Project
 
 projectslist=[
     {
@@ -22,20 +22,15 @@ projectslist=[
 #so to pass data to the html pages we pass them as dictionary in the render method
 #and we access them with {{page}} in the html pages
 def projects(request):
-    page="projects"
-    number=10
-    context ={'page':page,'number':number,'projects': projectslist}
+    projects=Project.objects.all()
+    context ={'projects': projects}
     return render(request, 'projects/projects.html',context) 
 
 
 #whatever paramter we name in the url path
 #the same name should be used as parameter in function
 def project(request,pk):
-    projectObj=None
-
-    for i in projectslist:
-        if i['id']==pk:
-            projectObj=i
-
     
+    projectObj= Project.objects.get(id=pk)
+    #tags=projectObj.tags.all()
     return render(request, 'projects/single-project.html',{'project':projectObj})  
