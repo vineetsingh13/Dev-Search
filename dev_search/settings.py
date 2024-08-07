b4b49ea1317bec333288412cc6185303a9ad1263
vineetsingh13 <singhvineet13502@gmail.com>
2024-08-08 01:23:01 +0530

    implemented some ui features

--- dev_search/settings.py
+++ dev_search/settings.py
@@ -133,6 +133,9 @@ STATICFILES_DIRS=[
 #FOR TELLING DJANGO WHERE TO STORE MEDIA CONTENTS
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
+#we use this because we want our static files to be availale when we host our website
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
