97aea8b64e667df7c3416736038c9e646fd59b99
vineetsingh13 <singhvineet13502@gmail.com>
2024-08-08 17:05:43 +0530

    added a loop in the forms.py in formtemplate to access each field and added a class as input for each field

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,10 @@
+from typing import Any, Mapping
+from django.core.files.base import File
+from django.db.models.base import Model
 from django.forms import ModelForm
+from django.forms.utils import ErrorList
 from .models import Project
+from django import forms
 
 #so basically what happens is using Django ModelForm we pass the model whose form we want to create and the fields we want
 #so based on the attribute type in models django automatically creates a form
@@ -10,3 +15,17 @@ class ProjectForm(ModelForm):
         model=Project
         fields=['title','featured_image', 'description','demo_link','source_link','tags']
         #fields='__all__'
+
+        #to choose the widget we want with each field in the form
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+    
+    def __init__(self,*args,**kwargs):
+        super(ProjectForm,self).__init__(*args,**kwargs)
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+
+        #for accessing each field in the form & update class, widget, etc
+        #self.fields['title'].widget.attrs.update({'class':'input','placeholder':'Add title'})
